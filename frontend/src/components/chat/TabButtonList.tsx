941a22749b96d4dcbad4b093c8aa8a12d423c5c3
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2026-07-14 16:44:21 +0700

    feat: enhance TabButtonList animation smoother

--- frontend/src/components/chat/TabButtonList.tsx
+++ frontend/src/components/chat/TabButtonList.tsx
@@ -5,7 +5,7 @@ import {
   MessageSquareMore,
   Settings,
   ChevronUp,
-  ChevronDown
+  ChevronDown,
 } from "lucide-react";
 import { TabButton, TabButtonInterface } from "../ui";
 import { usePathname } from "next/navigation";
@@ -43,14 +43,24 @@ const TabButtonList = () => {
   const pathname = usePathname();
   const [isCollapsed, setCollapsed] = useState(false);
   return (
-    <div className="flex flex-col justify-center w-full items-center border-b-[1.35] border-b-border-primary/70">
+    <div
+      className={cn(
+        "flex flex-col justify-center w-full items-center border-b-[1.35] border-b-border-primary/70",
+        isCollapsed && "border-none",
+      )}
+    >
       <div
         className={cn(
           "grid w-full transition-[grid-template-rows] duration-300 ease-in-out",
           isCollapsed ? "grid-rows-[0fr]" : "grid-rows-[1fr]",
         )}
       >
-        <ul className={cn("w-full px-3 flex flex-col gap-1 justify-center items-center overflow-hidden min-h-0", !isCollapsed && "py-1")}>
+        <ul
+          className={cn(
+            "w-full px-3 flex flex-col gap-1 justify-center items-center overflow-hidden min-h-0",
+            !isCollapsed && "pt-1 pb-3",
+          )}
+        >
           {tabButtons.map(({ index, href, icon, label }) => (
             <li key={index} className="w-full">
               <TabButton
@@ -66,7 +76,10 @@ const TabButtonList = () => {
 
       <button
         onClick={() => setCollapsed(!isCollapsed)}
-        className="group w-full px-3 py-px flex items-center justify-center text-gray-500 bg-black/6 hover:bg-black/9 hover:text-gray-800 active:bg-black/8 transition-colors duration-150 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-400"
+        className={cn(
+          "group w-full px-3 py-px flex items-center justify-center text-gray-500 bg-black/6 hover:bg-black/9 hover:text-gray-800 active:bg-black/8 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-400 transition-[width] duration-400 ease-in-out",
+          isCollapsed && "w-[30%] rounded-b-lg py-0",
+        )}
         aria-label={isCollapsed ? "Expand menu" : "Collapse menu"}
       >
         {isCollapsed ? (
